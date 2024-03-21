45a5ac1fae9ad375b49aa32b60f3bca1e5a51b6d
aj910 <anmolj2009@gmail.com>
2024-03-21 13:14:49 +0530

    Initial commit

---
+++ Intro/intro.js
@@ -0,0 +1,3 @@
+console.log("Hi Anmol");
+var user = { name: "Anmol", age: 29 };
+console.log(user.age);
---
+++ Intro/intro.ts
@@ -0,0 +1,4 @@
+console.log("Hi Anmol");
+
+const user = {name: "Anmol", age: 29};
+console.log(user.age);
