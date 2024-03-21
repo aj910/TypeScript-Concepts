e1fe2abba61a19ec431d92118c3300dce425d665
aj910 <anmolj2009@gmail.com>
2024-03-21 14:15:22 +0530

    variable working in TS

---
+++ Intro/variableTS.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "Hello Anmol!";
+console.log(message);
+// message = 10 // will throw an error: Type 'number' is not assignable to type 'string'
+var num = 10;
+console.log(num);
---
+++ Intro/variableTS.ts
@@ -0,0 +1,11 @@
+let message: string = "Hello Anmol!";
+
+console.log(message);
+
+// message = 10 // will throw an error: Type 'number' is not assignable to type 'string'
+
+const num: number = 10;
+console.log(num);
+//num.toUpperCase() // will throw an error: Property 'toUpperCase' does not exist on type 'number'
+
+export {} // temporarily added to remove redeclaration error of block scoped variables
