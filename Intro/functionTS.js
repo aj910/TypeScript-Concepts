36ecdc56b33272a216868fe479fd889b1fdc3e3a
aj910 <anmolj2009@gmail.com>
2024-03-28 20:20:33 +0530

    passing default values in TS functions

--- Intro/functionTS.js
+++ Intro/functionTS.js
@@ -3,3 +3,11 @@ function sum(a, b) {
 }
 var result = sum(10, 100);
 console.log(result);
+// passing default values in TS functions 
+function loginUser(name, email, isLoggedIn) {
+    if (isLoggedIn === void 0) { isLoggedIn = true; }
+    return "Login Details: " + name + email + isLoggedIn;
+}
+var show = loginUser("Anmol", "anmol@gmail.com");
+console.log(show);
+;
--- Intro/functionTS.ts
+++ Intro/functionTS.ts
@@ -5,6 +5,14 @@ function sum(a: number, b: number) {
 let result = sum(10, 100);
 console.log(result)
 
+// passing default values in TS functions 
+
+function loginUser(name: string, email: string, isLoggedIn: boolean = true) {
+    return "Login Details: " + "" + name + "" + email + "" + isLoggedIn
+}
+
+var show = loginUser("Anmol", "anmol@gmail.com");
+console.log(show);
 
 
 
